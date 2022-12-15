6c944d03f86329151f65c0421a6d7ab2f2d3b63c
Yulia Miller <miller82.98@mail.ru>
2022-12-15 03:04:22 -0800

    Слила два условия в одно (if)

--- EulerProject_Task_1/Program.cs
+++ EulerProject_Task_1/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace Task
+namespace mynamespase
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var sum = 0;
+            for(int i = 1; i < 10; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+                //else if ()
+                //{
+                //    sum += i;
+                //}
+                else
+                {
+                    continue;
 
+                }
+            }
+            Console.WriteLine("Сумма кратных 3 и 5 равна: {0}", sum);
+            Console.ReadKey();
         }
     }
 }
