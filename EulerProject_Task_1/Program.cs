85884540d2fe87c1e37675d06ffedf3d51296ef2
Yulia Miller <miller82.98@mail.ru>
2022-12-15 03:05:36 -0800

    Подвела к условию задачи сумма чисел < 1000 кратных 3 и 5

--- EulerProject_Task_1/Program.cs
+++ EulerProject_Task_1/Program.cs
@@ -7,20 +7,15 @@ namespace mynamespase
         public static void Main(string[] args)
         {
             var sum = 0;
-            for(int i = 1; i < 10; i++)
+            for(int i = 1; i < 100; i++)
             {
                 if (i % 3 == 0 || i % 5 == 0)
                 {
                     sum += i;
                 }
-                //else if ()
-                //{
-                //    sum += i;
-                //}
                 else
                 {
                     continue;
-
                 }
             }
             Console.WriteLine("Сумма кратных 3 и 5 равна: {0}", sum);
